eb052e183a7ba386a2a0fe95a17381ec2a15ce98
bbarkhouse-hashicorp <84409718+bbarkhouse-hashicorp@users.noreply.github.com>
2021-09-23 14:24:59 -0400

    fixed rg variables

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group.name
-  location = var.resource_group.location
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
 
 resource "azurerm_app_service_plan" "example" {
