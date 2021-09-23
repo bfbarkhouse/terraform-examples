36cb313ed212e9894fb3552c325fa927c1c77368
bbarkhouse-hashicorp <84409718+bbarkhouse-hashicorp@users.noreply.github.com>
2021-09-23 14:54:36 -0400

    fixed variable

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_app_service_plan" "example" {
-  name                = var.prefix + "app_service_plan"
+  name                = "${var.prefix}app-service-plan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
@@ -29,7 +29,7 @@ resource "azurerm_app_service_plan" "example" {
 }
 
 resource "azurerm_app_service" "example" {
-  name                = var.prefix + "app_service"
+  name                = "${var.prefix}app-service"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
