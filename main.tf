6dc58bba5f7fe2c15792ced94ecb5ea8957256fa
bbarkhouse-hashicorp <84409718+bbarkhouse-hashicorp@users.noreply.github.com>
2022-03-15 13:20:28 -0400

    changed code to create an aks cluster

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.77.0"
+      version = "=2.99.0"
     }
   }
 }
@@ -12,12 +12,42 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
 
-resource "azurerm_app_service_plan" "example" {
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.prefix}aks-dev-01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.prefix}"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+   identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+
+  sensitive = true
+}
+
+
+/* resource "azurerm_app_service_plan" "example" {
   name                = "${var.prefix}app-service-plan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
@@ -26,9 +56,9 @@ resource "azurerm_app_service_plan" "example" {
     tier = "Standard"
     size = "S1"
   }
-}
+} */
 
-resource "azurerm_app_service" "example" {
+/* resource "azurerm_app_service" "example" {
   name                = "${var.prefix}app-service"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
@@ -48,4 +78,4 @@ resource "azurerm_app_service" "example" {
     type  = "SQLServer"
     value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
   }
-}
+} */
