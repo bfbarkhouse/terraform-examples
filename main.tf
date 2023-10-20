8a8da849c9460e96f6b9bbb9e5ac9a9134d8a9e6
bbarkhouse-hashicorp <84409718+bbarkhouse-hashicorp@users.noreply.github.com>
2023-10-19 23:05:46 -0400

    fixing policy

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ resource "azurerm_public_ip" "public_ip" {
   location            = azurerm_resource_group.rg.location
   allocation_method   = "Dynamic"
 }
+
 resource "azurerm_public_ip" "public_ip2" {
   name                = "vm_public_ip2"
   resource_group_name = azurerm_resource_group.rg.name
