ff8ff7c80b516f2b0b0c20cbe98ce4e4d51db68d
bbarkhouse-hashicorp <84409718+bbarkhouse-hashicorp@users.noreply.github.com>
2023-10-23 09:28:29 -0400

    outputs

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "public_ip_address" {
   value = azurerm_public_ip.public_ip.ip_address
 }
-output "vm_identity" {
-    value = module.virtual-machine.azurerm_linux_virtual_machine.vm_linux.identity
+output "vm_identity_type" {
+    value = module.virtual-machine.vm_identity["type"]
 }
