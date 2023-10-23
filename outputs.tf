a9d8737d3150e7439e6d97b782ae4f1f8b89c1ce
bbarkhouse-hashicorp <84409718+bbarkhouse-hashicorp@users.noreply.github.com>
2023-10-23 09:35:45 -0400

    set up 2 outputs

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "public_ip_address" {
   value = azurerm_public_ip.public_ip.ip_address
 }
-output "vm_identity" {
-    value = module.virtual-machine.vm_identity[0]
+output "vm_admin_username" {
+    value = module.virtual-machine.vm_admin_username
 }
