b40d36857af3ba24836fb9cff1902cc1601be664
bbarkhouse-hashicorp <84409718+bbarkhouse-hashicorp@users.noreply.github.com>
2023-10-24 13:42:16 -0400

    fixing power state check

--- checks.tf
+++ checks.tf
@@ -2,19 +2,23 @@ check "health_check" {
   data "http" "terraform_io" {
     url = "https://www.terraform.io"
   }
-
   assert {
     condition = data.http.terraform_io.status_code == 200
     error_message = "${data.http.terraform_io.url} returned an unhealthy status code"
   }
 }
 
+
 check "check_vm_state" {
+  data "azurerm_virtual_machine" "linux_vm" {
+  name                = module.virtual-machine.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+}
   assert {
-    condition = data.azurerm_virtual_machine.power_state == "running"
+    condition = data.azurerm_virtual_machine.linux_vm.power_state == "running"
     error_message = format("Virtual Machine (%s) should be in a 'running' status, instead state is '%s'",
-      data.azurerm_virtual_machine.id,
-      data.azurerm_virtual_machine.power_state
+      data.azurerm_virtual_machine.linux_vm.id,
+      data.azurerm_virtual_machine.linux_vm.power_state
     )
   }
 }
