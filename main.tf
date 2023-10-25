20a7fae0da2ead1a022de09f448d30d30451969f
bbarkhouse-hashicorp <84409718+bbarkhouse-hashicorp@users.noreply.github.com>
2023-10-25 12:37:43 -0400

    revert

--- main.tf
+++ main.tf
@@ -42,17 +42,17 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-    security_rule {
-    name                       = "allow_http_sg"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+  #   security_rule {
+  #   name                       = "allow_http_sg"
+  #   priority                   = 100
+  #   direction                  = "Inbound"
+  #   access                     = "Allow"
+  #   protocol                   = "Tcp"
+  #   source_port_range          = "*"
+  #   destination_port_range     = "80"
+  #   source_address_prefix      = "*"
+  #   destination_address_prefix = "*"
+  # }
 }
 resource "azurerm_network_interface_security_group_association" "association" {
   network_interface_id      = module.virtual-machine.network_interface_id
