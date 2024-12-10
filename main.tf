470abfe0593c63da77d4f48ccaca9ba10ccc3c92
Ben Barkhouse <83463569+bfbarkhouse@users.noreply.github.com>
2024-12-10 16:18:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "azurerm" {
-  features {}
-skip_provider_registration = true
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.resource_group_location
